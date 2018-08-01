24469c303e50949db7a571adbc58d60f3db8cdb9
Jesse <jessekhorasanee@gmail.com>
2018-08-01 13:22:01 +1200

    changed web app to be build with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+var THREE = require('three')
+var $ = require('jquery')
+
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
